48e576a23073662f6098ceee82e060945525e2d2
Steven Wager <steven@Steven-Wagers-MacBook-Air.local>
2026-04-19 19:42:13 -0500

    Start of Day 5 project

--- Day-3.py
+++ Day-3.py
@@ -2,26 +2,26 @@ print("Welcome to treasure Island! Lets see if you can get to the treasure.")
 
 direction = input('You\'re at a forked road, would you like to go "left" or "right"? ').lower()
 
-if direction == "left":                          # Step 1: correct path
+if direction == "left":
     swim = input('You\'ve come to a river and see a bunch of bears! '
                  'Would you like to "swim" or "wait"? ').lower()
 
-    if swim == "wait":                           # Step 2: correct choice
+    if swim == "wait":
         door = input('After waiting out the bears you search along the river bed '
                      'and found some doors! What door will you take? '
                      'Blue, Red, or Yellow? ').lower()
 
-        if door == "yellow":                     # Step 3: correct door
+        if door == "yellow":
             print("You've happened upon a treasure chest! You've won!")
-        elif door == "red":                      # Step 3: wrong door
+        elif door == "red":
             print("You walked in on a dragon! Game over!")
-        elif door == "blue":                     # Step 3: wrong door
+        elif door == "blue":
             print("You walked in on the leviathan! Game over!")
-        else:                                    # Step 3: invalid input
+        else:
             print("You're not very good at treasure hunts. Goodbye!")
 
-    else:                                        # Step 2: wrong choice
+    else:
         print("The current of the river was too strong. Game Over!")
 
-else:                                            # Step 1: wrong path
+else:
     print("You've been bitten by a venomous snake. Game over!")
---
+++ Day-5.py
@@ -0,0 +1,16 @@
+# # student_scores = [ 180, 124, 165, 173, 189, 169, 146]
+# #
+# # max_score = 0
+# # for score in student_scores:
+# #     if score > max_score:
+# #         max_score = score
+# #         print(max_score)
+#
+#
+# total = 0
+# for number in range(1,101):
+#     total += number
+# print(total)
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '@']
